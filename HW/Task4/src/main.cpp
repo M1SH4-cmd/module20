5a05e83ea83ad0c97d5eef76538d28d5c0551c57
Михаил Беляков <misa.belyakov.ru@gmail.com>
2024-12-22 19:33:51 +0700

    Task4 done

--- HW/Task4/src/main.cpp
+++ HW/Task4/src/main.cpp
@@ -6,100 +6,64 @@
 
 using namespace std;
 
-const int MAX_BILLS = 1000;
-const vector<int> DENOMINATIONS = {5000, 2000, 1000, 500, 200, 100};
+int main()
+{
+    int bills[5] = {100, 200, 500, 1000, 5000};
+    int money[1000];
 
-int bills[6] = {0};
-int totalAmount = 0;
+    cout << "Operation:";
+    string op;
+    cin >> op;
 
-void loadATM() {
-    ifstream infile("atm_state.bin", ios::binary);
-    if (infile) {
-        for (int i = 0; i < 6; ++i) {
-            infile.read((char*) &bills[i], sizeof(bills[i]));
-        }
-        infile.read((char*) &totalAmount, sizeof(totalAmount));
-        infile.close();
+    ifstream file_in("..\\money.bin");
+    if (file_in.is_open()) {
+        file_in.read((char*) money, sizeof(money));
+        file_in.close();
     } else {
-        cout << "ATM is empty." << endl;
-    }
-}
-
-void saveATM() {
-    ofstream outfile("atm_state.bin", ios::binary);
-    for (int i = 0; i < 6; ++i) {
-        outfile.write((char*) &bills[i], sizeof(bills[i]));
-    }
-    outfile.write((char*) &totalAmount, sizeof(totalAmount));
-    outfile.close();
-}
-
-void fillATM() {
-    srand(time(0));
-    for (int i = 0; i < 6; ++i) {
-        int randomBills = rand() % (MAX_BILLS - totalAmount / DENOMINATIONS[i]) + 1;
-        bills[i] += randomBills;
-        totalAmount += randomBills * DENOMINATIONS[i];
-    }
-    cout << "ATM is filled." << endl;
-}
-
-bool withdraw(int amount) {
-    if (amount > totalAmount) {
-        cout << "Not enough money in the ATM." << endl;
-        return false;
-    }
-
-    for (int i = 0; i < 6; ++i) {
-        while (amount >= DENOMINATIONS[i] && bills[i] > 0) {
-            amount -= DENOMINATIONS[i];
-            bills[i]--;
-            totalAmount -= DENOMINATIONS[i];
+        for (int i = 0; i < 1000; ++i) {
+            money[i] = 0;
         }
     }
 
-    if (amount > 0) {
-        cout << "Unable to withdrawal this exact amount." << endl;
-        return false;
-    }
-
-    return true;
-}
-
-void printATMState() {
-    cout << "Current ATM state:" << endl;
-    for (int i = 0; i < 6; ++i) {
-        cout << DENOMINATIONS[i] << " rubles: " << bills[i] << endl;
-    }
-    cout << "Current sum: " << totalAmount << " rubles." << endl;
-}
-
-int main() {
-    loadATM();
-    printATMState();
-
-    string command;
-    while (true) {
-        cout << "Enter command (+ to input money, - for withdrawal, q to exit): ";
-        cin >> command;
-
-        if (command == "+") {
-            fillATM();
-            printATMState();
-        } else if (command == "-") {
-            int amount;
-            cout << "Enter sum for withdrawal: ";
-            cin >> amount;
-            if (withdraw(amount)) {
-                printATMState();
+    if (op == "+") {
+        for (int i = 0; i < 1000; ++i) {
+            if(money[i] == 0) {
+                money[i] = bills[rand() % 5];
             }
-        } else if (command == "q") {
-            saveATM();
-            break;
-        } else {
-            cout << "Incorrect command." << endl;
+        }
+        ofstream file_out("..\\money.bin");
+        file_out.write((char*) money, sizeof(money));
+        file_out.close();
+        return 0;
+    } else if(op == "-") {
+        cout << "Ammount: ";
+        int amount;
+        cin >> amount;
+        if((amount % 100) != 0) {
+            cout << "Invalid amount: " << amount << endl;
+            return 1;
+        }
+
+        int amountToCollect = amount;
+        for (int i = 4; i >= 0; --i) { // 
+            int bill = bills[i];
+            for (int j = 0; j < 1000; ++j) {
+                if (money[j] == bill) {
+                    if (amountToCollect >= bill) {
+                        money[j] = 0;
+                        amountToCollect -= bill;
+                        if (amountToCollect == 0) {
+                            cout << "Amount taken: " << amount << endl;
+                            ofstream file_out("..\\money.bin");
+                            file_out.write((char*) money, sizeof(money));
+                            file_out.close();
+                            return 0;
+                        }
+                    }
+                }
+            }
         }
     }
-
-    return 0;
+    cout << "Not enough compatible bills!" << endl;
+    return 1;
 }
